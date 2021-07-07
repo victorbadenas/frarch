91f0b8541eaa832659c65e8f75f5e70a724e4028
Victor Badenas <victor.badenas@gmail.com>
2021-07-07 19:19:59 +0200

    Initialise project from template repository

---
+++ frarch/__meta__.py
@@ -0,0 +1,12 @@
+# `name` is the name of the package as used for `pip install package`
+name = "frarch"
+# `path` is the name of the package for `import package`
+path = name.lower().replace("-", "_").replace(" ", "_")
+# Version number should follow https://python.org/dev/peps/pep-0440 and
+# https://semver.org
+version = "0.1.dev0"
+author = "vbadenas"
+author_email = "victor.badenas@gmail.com"
+description = "Training Framework for PyTorch projects"  # One-liner
+url = "https://github.com/vbadenas/frarch"  # your project homepage
+license = "MIT"  # See https://choosealicense.com
