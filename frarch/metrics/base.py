47ee065ebbeacce561007720751d090b12f1cd68
vbadenas <vbadenas@verbio.com>
2021-07-12 23:45:31 +0200

    controlled 0 len array

--- frarch/metrics/base.py
+++ frarch/metrics/base.py
@@ -12,11 +12,17 @@ class Metric:
     def update(self):
         raise NotImplementedError
 
+    def __len__(self):
+        return len(self.metrics)
+
     def get_metric(self, mode='mean'):
+        if len(self) == 0:
+            return .0
+
         if mode not in AGGREGATION_MODES:
             raise ValueError(f"Mode {mode} not supported. Supported modes: {AGGREGATION_MODES}")
         if mode == 'mean':
-            return sum(self.metrics) / len(self.metrics)
+            return sum(self.metrics) / len(self)
         elif mode == 'max':
             return max(self.metrics)
         elif mode == 'min':
