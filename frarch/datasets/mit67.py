53c8707070591816ce8512174c13e97c0ce2f406
Victor Badenas <victor.badenas@gmail.com>
2021-10-19 00:11:50 +0200

    formating changes

--- frarch/datasets/mit67.py
+++ frarch/datasets/mit67.py
@@ -1,29 +1,29 @@
 import csv
+import logging
 import os
 import os.path
 import tarfile
 from typing import Callable
 from urllib.parse import urlparse
 
-from torch.utils.data import Dataset
 from PIL import Image
+from torch.utils.data import Dataset
 
 from frarch.utils.data import download_url
-import logging
 
 logger = logging.getLogger(__name__)
 
 urls = {
-    'images': 'http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar',
-    'train_file': 'http://web.mit.edu/torralba/www/TrainImages.txt',
-    'test_file': 'http://web.mit.edu/torralba/www/TestImages.txt'
+    "images": "http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar",
+    "train_file": "http://web.mit.edu/torralba/www/TrainImages.txt",
+    "test_file": "http://web.mit.edu/torralba/www/TestImages.txt",
 }
 
 
 def download_mit(root):
     """Download the data."""
-    tmpdir = os.path.join(root, 'tmp')
-    path_images = os.path.join(root, 'Images')
+    tmpdir = os.path.join(root, "tmp")
+    path_images = os.path.join(root, "Images")
 
     # create directory
     if not os.path.exists(root):
@@ -36,45 +36,45 @@ def download_mit(root):
     if not os.path.exists(path_images):
 
         # download train/val images/annotations
-        parts = urlparse(urls['images'])
+        parts = urlparse(urls["images"])
         filename = os.path.basename(parts.path)
         cached_file = os.path.join(tmpdir, filename)
 
         if not os.path.exists(cached_file):
-            print('Downloading: "{}" to {}\n'.format(urls['images'], cached_file))
-            download_url(urls['images'], cached_file)
+            print('Downloading: "{}" to {}\n'.format(urls["images"], cached_file))
+            download_url(urls["images"], cached_file)
 
         # extract file
-        print(f'[dataset] Extracting tar file {cached_file} to {root}')
+        print(f"[dataset] Extracting tar file {cached_file} to {root}")
         cwd = os.getcwd()
         tar = tarfile.open(cached_file, "r")
         os.chdir(root)
         tar.extractall()
         tar.close()
         os.chdir(cwd)
-        print('[dataset] Done!')
+        print("[dataset] Done!")
 
     # download train file
-    parts = urlparse(urls['train_file'])
+    parts = urlparse(urls["train_file"])
     filename = os.path.basename(parts.path)
     cached_file = os.path.join(root, filename)
 
     if not os.path.exists(cached_file):
-        print('Downloading: "{}" to {}\n'.format(urls['train_file'], cached_file))
-        download_url(urls['train_file'], cached_file)
+        print('Downloading: "{}" to {}\n'.format(urls["train_file"], cached_file))
+        download_url(urls["train_file"], cached_file)
 
     # download test file
-    parts = urlparse(urls['test_file'])
+    parts = urlparse(urls["test_file"])
     filename = os.path.basename(parts.path)
     cached_file = os.path.join(root, filename)
 
     if not os.path.exists(cached_file):
-        print('Downloading: "{}" to {}\n'.format(urls['test_file'], cached_file))
-        download_url(urls['test_file'], cached_file)
+        print('Downloading: "{}" to {}\n'.format(urls["test_file"], cached_file))
+        download_url(urls["test_file"], cached_file)
 
 
 def find_classes(dir):
-    fname = os.path.join(dir, 'TrainImages.txt')
+    fname = os.path.join(dir, "TrainImages.txt")
     # read the content of the file
     with open(fname) as f:
         content = f.readlines()
@@ -98,10 +98,10 @@ def find_classes(dir):
 def make_dataset(dir, classes, set):
     images = []
 
-    if set == 'train':
-        fname = os.path.join(dir, 'TrainImages.txt')
-    elif set == 'test':
-        fname = os.path.join(dir, 'TestImages.txt')
+    if set == "train":
+        fname = os.path.join(dir, "TrainImages.txt")
+    elif set == "test":
+        fname = os.path.join(dir, "TestImages.txt")
     else:
         raise ValueError("set must be train or test")
 
@@ -121,30 +121,36 @@ def make_dataset(dir, classes, set):
 
 
 def write_csv_file(dir, images, set):
-    csv_file = os.path.join(dir, set + '.csv')
+    csv_file = os.path.join(dir, set + ".csv")
     if not os.path.exists(csv_file):
 
         # write a csv file
-        print('[dataset] write file %s' % csv_file)
-        with open(csv_file, 'w') as csvfile:
-            fieldnames = ['name', 'label']
+        print("[dataset] write file %s" % csv_file)
+        with open(csv_file, "w") as csvfile:
+            fieldnames = ["name", "label"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
             for x in images:
-                writer.writerow({'name': x[0], 'label': x[1]})
+                writer.writerow({"name": x[0], "label": x[1]})
 
         csvfile.close()
 
 
 class Mit67(Dataset):
-    def __init__(self, root, train: bool = True, transform: Callable = None,
-                 target_transform: Callable = None, download: bool = True):
+    def __init__(
+        self,
+        root,
+        train: bool = True,
+        transform: Callable = None,
+        target_transform: Callable = None,
+        download: bool = True,
+    ):
         self.root = root
         self.set = "train" if train else "test"
         self.transform = transform
         self.target_transform = target_transform
-        self.path_images = os.path.join(self.root, 'Images')
+        self.path_images = os.path.join(self.root, "Images")
 
         if download:
             download_mit(self.root)
@@ -160,14 +166,16 @@ class Mit67(Dataset):
             )
             raise e
 
-        print('[dataset] MIT67 set=%s  number of classes=%d  number of images=%d' % (
-            self.set, len(self.classes), len(self.images)))
+        print(
+            "[dataset] MIT67 set=%s  number of classes=%d  number of images=%d"
+            % (self.set, len(self.classes), len(self.images))
+        )
 
         write_csv_file(self.root, self.images, self.set)
 
     def __getitem__(self, index):
         path, target = self.images[index]
-        img = Image.open(os.path.join(self.path_images, path)).convert('RGB')
+        img = Image.open(os.path.join(self.path_images, path)).convert("RGB")
         if self.transform is not None:
             img = self.transform(img)
         if self.target_transform is not None:
