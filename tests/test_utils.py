3a4a06c92541f7764d60adf310c89310e57b95a8
vbadenas <victor.badenas@gmail.com>
2021-11-23 17:50:30 +0100

    string in comparison

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -120,7 +120,7 @@ class TestExceptions(unittest.TestCase):
     def test_dataset_not_found(self):
         path = Path("some_path")
         e = exceptions.DatasetNotFoundError(path)
-        self.assertIn(path, e.msg)
+        self.assertIn(str(path), e.msg)
 
     def test_dataset_not_found_raise(self):
         path = Path("some_path")
