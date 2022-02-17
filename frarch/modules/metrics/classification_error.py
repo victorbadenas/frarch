b6442f90a1f854fc7da9ab9dc3c49acae98e23b1
vbadenas <victor.badenas@midokura.com>
2022-02-17 21:59:21 +0100

    fixed metrics errors

--- frarch/modules/metrics/classification_error.py
+++ frarch/modules/metrics/classification_error.py
@@ -16,8 +16,8 @@ class ClassificationError(Metric):
             print(error.get_metric(mode="mean"))
     """
 
-    def _update(self, predictions: torch.Tensor, truth: torch.Tensor) -> None:
+    def _update(self, predictions: torch.Tensor, truth: torch.Tensor) -> torch.Tensor:
         if predictions.shape[0] != truth.shape[0]:
             raise ValueError(f"mismatched shapes {predictions.shape} != {truth.shape}")
         predictions = torch.argmax(predictions, dim=-1)
-        self.metrics.append((predictions != truth).float().mean().item())
+        return (predictions != truth).float().mean().item()
